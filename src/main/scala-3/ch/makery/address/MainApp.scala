fac28bcbe29195a9ebdadc477e3f633a4c75a3bc
GohIrene <22044614@imail.sunway.edu.my>
2025-07-24 20:52:37 +0800

    Practical 10062025

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,14 +1,11 @@
 package ch.makery.address
 
-import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
-import scalafx.beans.property.StringProperty
-import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
@@ -40,44 +37,3 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
-
-//  val stringA = new StringProperty("hello") //publisher
-//  val stringB = new StringProperty("sunway") //subscriber
-//  val stringC = new StringProperty("sunway") //subscriber
-//
-//  stringA.onChange { (_, oldValue, newValue)=>
-//    println(s"stringA changed from $oldValue to $newValue")
-//  }
-//  stringA.onChange { (_, _, _) =>
-//    println(s"stringA has change ")
-//  }
-//  stringA.value = "world"
-//
-//  val func1: Int => Int =(_) =>
-//    1
-//  println(func1(8))
-//
-//  stringB <==> stringA
-//  stringC <== stringA
-//  stringB.value = "google"
-//  println(stringA.value)
-//
-//  println(stringB)
-
-/**
- * The data as an observable list of Persons.
- */
-val personData = new ObservableBuffer[Person]()
-/**
- * Constructor
- */
-personData += new Person("Hans", "Muster")
-personData += new Person("Ruth", "Mueller")
-personData += new Person("Heinz", "Kurz")
-personData += new Person("Cornelia", "Meier")
-personData += new Person("Werner", "Meyer")
-personData += new Person("Lydia", "Kunz")
-personData += new Person("Anna", "Best")
-personData += new Person("Stefan", "Meier")
-personData += new Person("Martin", "Mueller")
- 
