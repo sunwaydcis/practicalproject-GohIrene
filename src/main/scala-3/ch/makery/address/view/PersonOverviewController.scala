9f6f9f07d6a559090e490f5d9d657eae2b274a0d
GohIrene <22044614@imail.sunway.edu.my>
2025-07-26 01:45:44 +0800

    Part 3b

--- src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
+++ src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
@@ -81,9 +81,6 @@ class PersonOverviewController():
         cityLabel.text = ""
         birthdayLabel.text = ""
 
-  /**
-   * Called when the user clicks on the delete button.
-   */
   // handle delete person
   // find user click what button in the action event
   @FXML //anything with fxml, is using javafx library
@@ -102,6 +99,34 @@ class PersonOverviewController():
         headerText = "No Person Selected"
         contentText = "Please select a person in the table."
       alert.showAndWait()
+
+  @FXML
+  // Handle the dialog box
+  def handleNewPerson(action: ActionEvent) =
+    val person = new Person("", "")
+    val okClicked = MainApp.showPersonEditDialog(person);
+    if (okClicked) then
+      MainApp.personData += person
+
+  @FXML
+  def handleEditPerson(action: ActionEvent) =
+    val selectedPerson = personTable.selectionModel().selectedItem.value
+    if (selectedPerson != null) then
+      val okClicked = MainApp.showPersonEditDialog(selectedPerson)
+
+      if (okClicked) then showPersonDetails(Some(selectedPerson))
+
+    else
+      // Nothing selected.
+      val alert = new Alert(Alert.AlertType.Warning):
+        initOwner(MainApp.stage)
+        title = "No Selection"
+        headerText = "No Person Selected"
+        contentText = "Please select a person in the table."
+      .showAndWait()
+
+
+
 
 
 
